5d41bfafeeb888ea12a0a776e95e390197024ab8
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-14 00:39:27 -0300

    Update declare-variables(1).py

--- declare-variables(1).py
+++ declare-variables(1).py
@@ -4,3 +4,5 @@
 x = 1
 #First at all you will need to give a name to your variable, in the exemple above "x" is the name of the variable, and the signal "=" means that your variable
 #will receive some value, so if "x = 1", the variable "x" receive the number one
+
+#**ATTENTION: The variable name can´t have space, start with numbers or special characters
