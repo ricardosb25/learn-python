ba773385788e159217d84d118c6b2aa56121daf0
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-14 00:34:33 -0300

    Create types-of-variables.py

---
+++ types-of-variables.py
@@ -0,0 +1,18 @@
+#DIFFERENT TYPES OF VARIABLES
+
+#There tree basic types of variable: int,float and char.
+
+x = 1
+
+#In the exemple above, the variable "x" receive an INT(integers) number
+
+y = 2.5
+
+#In the exemple above, the variable "x" receive an FLOAT(actual figure) number which is always seperate by a "." dot
+
+name = "Ricardo"
+
+#In the exemple above, the variable "x" receive an CHAR(character) which could be a word or phrases and is always between """ quotations marks
+
+#That is the most important variables in python
+
