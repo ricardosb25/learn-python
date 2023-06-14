66aac02e9c3cab800320c2de6afea28069e24f8a
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-14 00:21:23 -0300

    Create declare-variables(1).py

---
+++ declare-variables(1).py
@@ -0,0 +1,6 @@
+#HOW TO THE DECLARE VARIABLES
+
+#The first you will need to learn python is how to declare variables
+x = 1
+#First at all you will need to give a name to your variable, in the exemple above "x" is the name of the variable, and the signal "=" means that your variable
+#will receive some value, so if "x = 1", the variable "x" receive the number one
